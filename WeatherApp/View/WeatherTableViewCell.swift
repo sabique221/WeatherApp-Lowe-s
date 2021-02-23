d70a208824c04955bd1c0eb316b2d29cfbfcf11b
Sabique Islam <sabik.roauf@gmail.com>
2021-02-23 16:20:39 -0500

    last

--- WeatherApp/View/WeatherTableViewCell.swift
+++ WeatherApp/View/WeatherTableViewCell.swift
@@ -22,7 +22,6 @@ class WeatherTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        backgroundColor = UIColor.white
     }
     
     func cellConfiguration() {
